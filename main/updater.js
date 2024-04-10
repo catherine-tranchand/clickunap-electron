92dc0cd6c52f118ea5d74c33808d58a5a33d2863
abraham-ukachi <abraham.ukachi@laplateforme.io>
2024-04-10 03:39:36 +0200

    :bug: Fix auto updater 2/2

--- main/updater.js
+++ main/updater.js
@@ -1,6 +1,13 @@
 const { dialog } = require('electron');
 const log = require('electron-log');
+const path = require('path');
 const { autoUpdater } = require('electron-updater');
+
+
+
+
+
+
 
 const checkForUpdates = (win = null) => {
   /*
@@ -9,6 +16,10 @@ const checkForUpdates = (win = null) => {
   }
   */
 
+
+
+
+
   // =================  Logging =====================
   autoUpdater.logger = log;
   autoUpdater.logger.transports.file.level = 'info';
@@ -16,11 +27,26 @@ const checkForUpdates = (win = null) => {
   // ================================================
 
 
+  /**
+   * Function used to send the given `text` status to the window (i.e. `win`)
+   *
+   * @param { String } text
+   *
+   * @returns { void } - nothing ;)
+   */
+  const sendStatusToWindow = (text) => {
+    // do nothing if there's no `win`
+    if (!win) return;
+
+    log.info(text);
+    win.webContents.send('message', text);
+  };
+  
+
 
   const updateUrl = 'https://github.com/catherine-tranchand/clickunap-electron/releases/latest';
 
   autoUpdater.setFeedURL(updateUrl);
-
   autoUpdater.autoDownload = false;
   autoUpdater.autoInstallOnAppQuit = false;
   
@@ -28,23 +54,22 @@ const checkForUpdates = (win = null) => {
   // check for updates every 10 minutes
   const updateInterval = setInterval(() => {
     autoUpdater.checkForUpdates();
-  }, 1000 * 60 * 10);
+    sendStatusToWindow('[updateInterval]: check for updates every 60 seconds');
+  }, 1000 * 60); // <- every 60 sconds | 10 minutes = 1000 * 60 * 10
   
 
   // >> Checking for update
   autoUpdater.on('checking-for-update', () => {
-    console.log('Checking for update...');
-    log.info('Checking for update...');
+    sendStatusToWindow('Checking for update...');
   });
   
   // >> Update available 
   autoUpdater.on('update-available', (info) => {
-    console.log('Update available', info);
-    log.info('Update available', info);
+    sendStatusToWindow('Update available', info);
 
     dialog.showMessageBox({
       type: 'info',
-      title: 'Update Available',
+      title: 'Clickunap Update Available',
       message: `A new version ${info.version} is available. Do you want to update?`,
       buttons: ['Yes', 'No']
     }).then(result => {
@@ -56,26 +81,27 @@ const checkForUpdates = (win = null) => {
   
   // >> Update not available
   autoUpdater.on('update-not-available', (info) => {
-    console.log('Update not available', info);
-    log.info('Update not available', info);
+    sendStatusToWindow('Update not available');
   });
   
   // >> Error
   autoUpdater.on('error', (error) => {
-    console.error('Update error:', error);
-    log.error('Update error:', error);
+    sendStatusToWindow('Error in auto-updater: ' + error);
   });
   
   // >> Download progress
   autoUpdater.on('download-progress', (progressObj) => {
-    console.log('Download progress:', progressObj);
-    log.info('Download progress:', progressObj);
-  });
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
 
+    sendStatusToWindow(log_message);
+  });
+  
   
   // >> Update downloaded
   autoUpdater.on('update-downloaded', (info) => {
-    console.log('Update downloaded', info);
+    sendStatusToWindow('Update downloaded', info);
     clearInterval(updateInterval);
 
     dialog.showMessageBox({
