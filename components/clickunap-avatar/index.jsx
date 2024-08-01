b1c9b55cce12fa9078b5090cdeb6403f2b2e6f4a
abraham-ukachi <abraham.ukachi@laplateforme.io>
2024-08-01 17:43:41 +0200

    :test_tube: Test as prop to fix image warning

--- components/clickunap-avatar/index.jsx
+++ components/clickunap-avatar/index.jsx
@@ -1,16 +1,20 @@
 import clsx from 'clsx'
 import Image from 'next/image'
 
-export default function ClickunapAvatar({ id, size, className, style }) {
+export default function ClickunapAvatar({ id, size, className, selected, style, onClick }) {
   return (
     <Image 
-      className={clsx(['ClickunapAvatar', 'flex justify-center items-center rounded-full p-2 size-6 overflow-hidden object-cover relative', className])}
+      className={clsx(['ClickunapAvatar', 'flex justify-center items-center rounded-full p-2 size-6 overflow-hidden object-cover relative', className],
+        {'outline outline-4 outline-offset-2 outline-tertiary': selected}
+      )}
       style={{style}}
       src={getSvgById(id)}
       width={size ?? 24}
       height={size ?? 24}
       alt="Clickunap avatar"
-      priority={true}
+      priority={false}
+      as="image"
+      onClick={onClick}
     />
   )
 
