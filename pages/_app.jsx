7b6c94541b127092bcff16170fe627e4f948e4dc
abraham-ukachi <abraham.ukachi@laplateforme.io>
2025-03-30 04:42:02 +0200

    :boom: create dashboard overview

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -10,28 +10,9 @@ import Image from "next/image";
 import RootLayout from "@/layout";
 import "@/styles/globals.css";
 
-import { createTheme, ThemeProvider, alpha, getContrastRatio } from '@mui/material/styles';
+import ClickunapThemeProvider from '@/providers/ClickunapThemeProvider';
 import UserProvider from "@/providers/UserProvider";
 import useStorage from "@/hooks/useStorage";
-
-
-
-const primaryBase = "#745077";
-const primaryMain = alpha(primaryBase, 1);
-
-
-const theme = createTheme({
-  palette: {
-    // mode: 'dark',
-    primary: {
-      main: primaryMain,
-      light: alpha(primaryBase, 0.5),
-      dark: alpha(primaryBase, 0.9),
-      contrastText: getContrastRatio(primaryMain, "#fff") > 4.5 ? "#fff" : "#111",
-    }
-  },
-});
-
 
 
 // constants
@@ -82,7 +63,7 @@ export default function App({ Component, pageProps }) {
 
   return (
     <UserProvider initUserToken={userToken}>
-      <ThemeProvider theme={theme}>
+      <ClickunapThemeProvider>
         <RootLayout>
 
         {isLoading && (
@@ -104,7 +85,7 @@ export default function App({ Component, pageProps }) {
         )}
 
         </RootLayout>
-      </ThemeProvider>
+      </ClickunapThemeProvider>
     </UserProvider>
   )
 }
